255dbdaf7b36b6626d87d92177ca7ee4d1f8465d
kay27 <kay27@bk.ru>
2013-03-13 13:22:48 +0400

    template.html & quote.txt are being cached

--- openmcu_45/html.cxx
+++ openmcu_45/html.cxx
@@ -11,96 +11,138 @@ PString Browser_Language(PHTTPRequest & request)
   return "EN";
 }
 
+static long html_template_size; // count on zero initialization
+char * html_template_buffer;
+static long html_quote_size; // count on zero initialization
+char * html_quote_buffer;
+PMutex html_mutex;
+
 void BeginPage (PStringStream &html, const char *ptitle, const char *title, const char *quotekey) 
 {
-  long p_size;
-  char * p_buffer;
-  long q_size;
-  char * q_buffer;
+  PWaitAndSignal m(html_mutex);
 
-  FILE *fs;
-
-  fs=fopen("template.html","r"); if(!fs) return;
-  fseek(fs,0L,SEEK_END); p_size=ftell(fs); rewind(fs);
-  p_buffer=new char[p_size+1];
-  fread(p_buffer,1,p_size,fs); p_buffer[p_size]=0; 
-  fclose(fs);
-
-  fs=fopen("quote.txt","r"); if(!fs) { free(p_buffer); return; }
-  fseek(fs,0L,SEEK_END); q_size=ftell(fs); rewind(fs);
-  q_buffer=new char[q_size+1]; q_buffer[q_size]=0;
-  fread(q_buffer,1,q_size,fs);
-  fclose(fs);
-  
-  char *ptt=strstr(p_buffer,"$PTITLE$");
-  if(ptt)
+  if(html_template_size == 0) // count on zero initialization
   {
-   *ptt=0; ptt+=8;
-   html << p_buffer << ptitle;
-  }
-  else ptt=p_buffer;
-
-  char *tt=strstr(ptt,"$TITLE$");
-  if(tt)
-  {
-   *tt=0; tt+=7;
-   html << ptt << title;
-  }
-  else tt=ptt;
-
-  char *quote_beg=strstr(q_buffer,quotekey);
-  if(quote_beg)
-  {
-   quote_beg+=strlen(quotekey);
-   char *quote_end=strstr(quote_beg,"$$");
-   if(quote_end) *quote_end=0;
-  }
-  else quote_beg=q_buffer;
-  
-  char *qt=strstr(tt,"$QUOTE$");
-  if(qt)
-  {
-   *qt=0; qt+=7;
-   html << tt << quote_beg;
-  }
-  else qt=tt;
-  
-  char *bt=strstr(qt,"$BODY$");
-  if(bt)
-  {
-   *bt=0;
-   html << qt;
+    FILE *fs;
+    fs=fopen("template.html", "r");
+    if(fs)
+    {
+      fseek(fs, 0L, SEEK_END);
+      html_template_size = ftell(fs);
+      rewind(fs);
+      html_template_buffer = new char[html_template_size + 1];
+      fread(html_template_buffer, 1, html_template_size, fs);
+      html_template_buffer[html_template_size] = 0;
+      fclose(fs);
+    }
+    else
+    {
+      html_template_size = -1; // read error indicator
+    }
   }
 
-  free(p_buffer);
-  free(q_buffer);
+  if(html_quote_size == 0) // count on zero initialization
+  {
+    FILE *fs;
+    fs=fopen("quote.txt", "r");
+    if(fs)
+    {
+      fseek(fs, 0L, SEEK_END);
+      html_quote_size = ftell(fs);
+      rewind(fs);
+      html_quote_buffer = new char[html_quote_size + 1];
+      fread(html_quote_buffer, 1, html_quote_size, fs);
+      html_quote_buffer[html_quote_size] = 0;
+      fclose(fs);
+    }
+    else
+    {
+      html_quote_size = -1; // read error indicator
+    }
+  }
+
+  if ((html_template_size > 0) && (html_quote_size > 0))
+  {
+    char *ptt = strstr(html_template_buffer, "$PTITLE$");
+    if(ptt)
+    {
+      *ptt=0;
+      html << html_template_buffer << ptitle;
+      *ptt='$';
+      ptt+=8;
+    }
+    else ptt = html_template_buffer;
+
+    char *tt=strstr(ptt, "$TITLE$");
+    if(tt)
+    {
+      *tt=0;
+      html << ptt << title;
+      *tt='$';
+      tt+=7;
+    }
+    else tt=ptt;
+
+    char *quote_beg = strstr(html_quote_buffer, quotekey); // searching key here
+    char *quote_end = NULL;
+    BOOL needs_restore = FALSE;
+
+    if(quote_beg)
+    {
+      quote_beg += strlen(quotekey);
+      quote_end = strstr(quote_beg, "$$");
+      if(quote_end)
+      {
+        *quote_end=0;
+        needs_restore = TRUE;
+      }
+    }
+    else quote_beg = html_quote_buffer;
+
+    char *qt = strstr(tt, "$QUOTE$");
+    if(qt)
+    {
+      *qt=0;
+      html << tt << quote_beg;
+      *qt='$';
+      qt+=7;
+    }
+    else qt=tt;
+    if(needs_restore) *quote_end='$';
+
+    char *bt = strstr(qt,"$BODY$");
+    if(bt)
+    {
+      *bt=0;
+      html << qt;
+      *bt='$';
+    }
+
+  } //  if ((html_template_size > 0) && (html_quote_size > 0))
+
 }
 
 void EndPage (PStringStream &html, PString copyr) 
 {
-  long p_size;
-  char * p_buffer;
-  FILE *fs;
+  PWaitAndSignal m(html_mutex);
 
-  fs=fopen("template.html","r"); if(!fs) return;
-  fseek(fs,0L,SEEK_END); p_size=ftell(fs); rewind(fs);
-  p_buffer=new char[p_size];
-  fread(p_buffer,1,p_size,fs); p_buffer[p_size]=0;
-  fclose(fs);
-
-  char *bt=strstr(p_buffer,"$BODY$");
-  if(bt)
+  if ((html_template_size > 0) && (html_quote_size > 0))
   {
-   bt+=6;
-   char *ct=strstr(bt,"$COPYRIGHT$");
-   if(ct)
-   {
-    *ct=0; ct+=11;
-    html << bt << copyr << ct;
-   }
+    char *bt = strstr(html_template_buffer,"$BODY$");
+    if(bt)
+    {
+      bt+=6;
+      char *ct=strstr(bt,"$COPYRIGHT$");
+      if(ct)
+      {
+        *ct=0;
+        html << bt << copyr;
+        *ct='$';
+        ct+=11;
+        html << ct;
+      }
+    }
   }
-
-  free(p_buffer);
 }
 
 PString ErrorPage( //maybe ptlib could already create pages like this? for future: dig into http server part
