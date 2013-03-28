6bf92bf3fb85883abf51c83c621e27207d7f102d
kay27 <kay27@bk.ru>
2013-03-28 13:39:46 +0400

    script for broadcast via ffmpeg.exe->vlc.exe

--- openmcu_45/conf/web_stream_start.cmd
+++ openmcu_45/conf/web_stream_start.cmd
@@ -5,10 +5,10 @@
 set room=%1
 if "%room%"=="" set room=room101
 
-set rname=%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%_%room%
-
 ffmpeg -y^
  -f s16le -ac 1 -ar 16000 -i \\.\pipe\sound_%room%^
  -f rawvideo -r %framerate% -s %frame% -i \\.\pipe\video_%room%^
- -f asf -acodec pcm_s16le -vcodec msmpeg4v2^
- "%rname%.asf"
+ -f mpegts^
+ -acodec mp2 -ab 32k^
+ -vcodec mpeg2video -b 384k^
+ - | "%ProgramFiles%\VideoLAN\VLC\vlc.exe" - --sout=#standard{access=http,mux=ts,dst=:8090}
