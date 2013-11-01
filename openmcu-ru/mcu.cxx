817db43a667828826e2b36bb49469b4cfa33899b
Andrey Burbovskiy, xak-mcu <xak-altsp@yandex.ru>
2013-11-01 19:38:31 +0400

    left menu image

--- openmcu-ru/mcu.cxx
+++ openmcu-ru/mcu.cxx
@@ -358,6 +358,7 @@ BOOL OpenMCU::Initialise(const char * initMsg)
   WEBSERVER_LINK_MIME("image/gif"                , "i24_revert.gif");
   WEBSERVER_LINK_MIME("image/vnd.microsoft.icon" , "openmcu.ico");
   WEBSERVER_LINK_MIME("image/x-windows-bmp"      , "openmcu.ru_logo_text.bmp");
+  WEBSERVER_LINK_MIME("image/png"                , "menu_left.png");
 
   // set up the HTTP port for listening & start the first HTTP thread
   if (ListenForHTTP((WORD)cfg.GetInteger(HttpPortKey, DefaultHTTPPort)))
