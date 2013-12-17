ded856304f1719ae9a73cc86c605f84f1adfe172
kay27 <kay27@bk.ru>
2013-12-17 17:26:21 +0400

    version changed to 3.46.0 (stable)

--- openmcu-ru/version.h
+++ openmcu-ru/version.h
@@ -3,9 +3,9 @@
 #define _OpenMCU_VERSION_H
 
 #define MAJOR_VERSION 3
-#define MINOR_VERSION 1
+#define MINOR_VERSION 46
 #define BUILD_TYPE    ReleaseCode
-#define BUILD_NUMBER  1
+#define BUILD_NUMBER  0
 
 
 #endif  // _OpenMCU_VERSION_H
