2b9350878bd829674e6da877807e0f8b51d42089
Konstantin Yeliseyev <kay27@bk.ru>
2014-05-02 03:32:49 +0400

    v3.48.0

--- openmcu-ru/version.h
+++ openmcu-ru/version.h
@@ -3,9 +3,9 @@
 #define _OpenMCU_VERSION_H
 
 #define MAJOR_VERSION 3
-#define MINOR_VERSION 47
+#define MINOR_VERSION 48
 #define BUILD_TYPE    ReleaseCode
-#define BUILD_NUMBER  28
+#define BUILD_NUMBER  0
 
 
 #endif  // _OpenMCU_VERSION_H
