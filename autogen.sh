762f048436e6932e9ff6bea7af065c314a8bce3a
Andrey Burbovskiy <xak-altsp@yandex.ru>
2014-06-16 15:56:28 +0400

    addition 1a089dab40b643b2e3962e8aacf82dd3b0e1ec54

--- autogen.sh
+++ autogen.sh
@@ -5,5 +5,7 @@ aclocal
 autoconf
 automake --add-missing --copy
 
-cd libs/libzrtp/ && ./bootstrap.sh
+top_srcdir=`pwd`
 
+cd ${top_srcdir}/libs/libzrtp/ && ./bootstrap.sh
+cd ${top_srcdir}/plugins/G722.1/libg722_1/ && ./autogen.sh
