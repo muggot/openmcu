7e0847f5be771d90142d863afeb3f1063bf3bbfe
Andrey Burbovskiy <xak-altsp@yandex.ru>
2015-01-23 20:30:10 +0400

    addition 7240e2981c3c3d1e2062f43522bd84451a9a5bb0

--- autogen.sh
+++ autogen.sh
@@ -7,10 +7,6 @@ automake --add-missing --copy
 
 top_srcdir=`pwd`
 
-if [ -d ${top_srcdir}/libs/libzrtp ] ; then
-  cd ${top_srcdir}/libs/libzrtp/ && ./bootstrap.sh
-fi
-
 if [ -d ${top_srcdir}/plugins/G722.1/libg722_1 ] ; then
   cd ${top_srcdir}/plugins/G722.1/libg722_1/ && ./autogen.sh
 fi
