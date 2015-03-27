11f37580a8464e46a08bd02d605634ae20712cc5
Konstantin Yeliseyev <kay27@bk.ru>
2015-03-28 00:03:52 +0300

    better icon behavior: room ctrl page -> invive from top panel

--- openmcu-ru/files/resource/control.js
+++ openmcu-ru/files/resource/control.js
@@ -429,7 +429,9 @@ function inviteoffline(obj,mname,add)
   if(typeof add == 'undefined') add=0;
   if(add)queue_otf_request(OTFC_ADD_AND_INVITE,mname);
   else queue_otf_request(OTFC_INVITE,mname);
-  obj.src='launched.gif';
+  if(obj)
+    if(obj.className=='topdspr') obj.className='topdspr_w';
+    else obj.src='launched.gif';
 }
 
 function notice_deletion(stage, room)
@@ -980,8 +982,9 @@ function invite_panel(){
   var b1x = input_posx + input_width;
   var b2x = b1x + bwidth;
   s+=dpre+proto_posx+"px'><div id='divInvProto' class='btn' style='font-size:12px;width:"+proto_width+"px;height:20px;padding:0px;border-radius:0px;' onclick='javascript:{if(this.innerHTML==\"h323\")this.innerHTML=\"rtsp\";else if(this.innerHTML==\"rtsp\")this.innerHTML=\"sip\";else if(this.innerHTML==\"sip\")this.innerHTML=\"h323\";document.getElementById(\"invite_input\").focus();}'>"+get_default_proto()+"</div></div>";
-  s+=dpre+input_posx+"px'><input id='invite_input' type='text' style='font-size:12px;width:"+input_width+"px;height:20px;padding:0px;border-radius:0px;border-right:0px;' onkeyup='javascript:{if(mlgctr1){document.getElementById(\"binpinv\").src=\"i15_inv.gif\";mlgctr1=0;};if(event.keyCode==13){dial_from_input(document.getElementById(\"binpinv\"));mlgctr1=1;}}' /></div>";
-  s+=dpre+b1x+"px'>"+dbutton+"width:"+(bwidth)+"px;' onclick='dial_from_input(this)'><img id='binpinv' style='cursor:pointer' src='i15_inv.gif' width="+width+" height="+height+" title='"+window.l_invite+"' /></div></div>";
+  s+=dpre+input_posx+"px'><input id='invite_input' type='text' style='font-size:12px;width:"+input_width+"px;height:20px;padding:0px;border-radius:0px;border-right:0px;' onkeyup='javascript:{if(mlgctr1){document.getElementById(\"binpinv\").className=\"topdspr\";mlgctr1=0;};if(event.keyCode==13){mlgctr1=1;dial_from_input(document.getElementById(\"binpinv\"));}}' /></div>";
+  s+=dpre+b1x+"px'>"+dbutton+"width:"+(bwidth)+"px' onclick='mlgctr1=1;dial_from_input(document.getElementById(\"binpinv\"));'>"
+    +"<div class='topdspr' id='binpinv' title='"+window.l_invite+"'></div></div></div>";
   s+=dpre+b2x+"px'>"+dbutton+"width:"+(bwidth)+"px;' onclick='add_to_abook_input()'><img src='i16_abook_plus.png' title='"+window.l_add_to_abook+"' /></div></div>";
   s+="</form>";
   return s;
